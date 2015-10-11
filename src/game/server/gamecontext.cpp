9f6d800628a2489bce406ac4534cfc311c297e37
nheir <henri.derycke@ens-lyon.org>
2015-10-11 22:24:16 +0200

    Don't count bots in vote

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -481,7 +481,7 @@ void CGameContext::OnTick()
 				bool aVoteChecked[MAX_CLIENTS] = {0};
 				for(int i = 0; i < MAX_CLIENTS; i++)
 				{
-					if(!m_apPlayers[i] || m_apPlayers[i]->GetTeam() == TEAM_SPECTATORS || aVoteChecked[i])	// don't count in votes by spectators
+					if(!m_apPlayers[i] || m_apPlayers[i]->GetTeam() == TEAM_SPECTATORS || aVoteChecked[i] || m_apPlayers[i]->m_IsBot)	// don't count in votes by spectators
 						continue;
 
 					int ActVote = m_apPlayers[i]->m_Vote;
