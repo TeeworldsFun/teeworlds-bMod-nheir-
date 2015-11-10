c8536267cd5f82f4a5fdde99f71e2bc6961a983b
Henri Derycke <henri.derycke@u-bordeaux.fr>
2015-11-10 19:09:40 +0100

    Fix wrong return value of IntersectSegment

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -68,60 +68,66 @@ void CCollision::BuildSegments()
 {
 	int VSegmentCount = 0;
 	int HSegmentCount = 0;
+	// We start counting segments
+	// Horizontal segments
+	for(int j = 1; j < m_Height-1; j++)
+	{
+		bool top = false;
+		bool bottom = false;
+		for(int i = 0; i < m_Width; i++)
+		{
+			// Segment on the bottom of line j
+			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+(j+1)*m_Width].m_Index <= 128 && m_pTiles[i+(j+1)*m_Width].m_Index & COLFLAG_SOLID))
+				bottom = true;
+			else if(bottom)
+			{
+				HSegmentCount++;
+				bottom = false;
+			}
+
+			// Segment on the top of line j
+			if(((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+(j-1)*m_Width].m_Index <= 128 && m_pTiles[i+(j-1)*m_Width].m_Index & COLFLAG_SOLID)))
+				top = true;
+			else if(top)
+			{
+				HSegmentCount++;
+				top = false;
+			}
+		}
+		if(bottom)
+			HSegmentCount++;
+		if(top)
+			HSegmentCount++;
+	}
 	// Vertical segments
 	for(int i = 1;i < m_Width-1; i++)
 	{
 		bool right = false;
 		bool left = false;
-		for(int j = 1; j < m_Height-1; j++)
+		for(int j = 0; j < m_Height; j++)
 		{
+			// Segment on the right side of column i
 			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+1+j*m_Width].m_Index <= 128 && m_pTiles[i+1+j*m_Width].m_Index & COLFLAG_SOLID))
-				left = true;
-			else if(left)
-			{
-				VSegmentCount++;
-				left = false;
-			}
-			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i-1+j*m_Width].m_Index <= 128 && m_pTiles[i-1+j*m_Width].m_Index & COLFLAG_SOLID))
 				right = true;
 			else if(right)
 			{
 				VSegmentCount++;
 				right = false;
 			}
+
+			// Segment on the left side of column i
+			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i-1+j*m_Width].m_Index <= 128 && m_pTiles[i-1+j*m_Width].m_Index & COLFLAG_SOLID))
+				left = true;
+			else if(left)
+			{
+				VSegmentCount++;
+				left = false;
+			}
 		}
-		if(left)
-			VSegmentCount++;
 		if(right)
 			VSegmentCount++;
-	}
-	// Horizontal segments
-	for(int j = 1; j < m_Height-1; j++)
-	{
-		bool up = false;
-		bool down = false;
-		for(int i = 1; i < m_Width-1; i++)
-		{
-			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+(j+1)*m_Width].m_Index <= 128 && m_pTiles[i+(j+1)*m_Width].m_Index & COLFLAG_SOLID))
-				up = true;
-			else if(up)
-			{
-				HSegmentCount++;
-				up = false;
-			}
-
-			if(((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+(j-1)*m_Width].m_Index <= 128 && m_pTiles[i+(j-1)*m_Width].m_Index & COLFLAG_SOLID)))
-				down = true;
-			else if(down)
-			{
-				HSegmentCount++;
-				down = false;
-			}
-		}
-		if(up)
-			HSegmentCount++;
-		if(down)
-			HSegmentCount++;
+		if(left)
+			VSegmentCount++;
 	}
 	m_SegmentCount = HSegmentCount+VSegmentCount;
 	m_HSegmentCount = HSegmentCount;
@@ -133,90 +139,78 @@ void CCollision::BuildSegments()
 	// Horizontal segments
 	for(int j = 1; j < m_Height-1; j++)
 	{
-		bool up = false;
-		bool down = false;
-		int up_i, down_i;
+		bool bottom = false;
+		bool top = false;
+		int bottom_i, top_i;
 		for(int i = 0; i < m_Width; i++)
 		{
+			// Segment on the bottom of line j
 			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+(j+1)*m_Width].m_Index <= 128 && m_pTiles[i+(j+1)*m_Width].m_Index & COLFLAG_SOLID))
 			{
-				if(!up)
-					up_i = i;
-				up = true;
+				if(!bottom)
+					bottom_i = i;
+				bottom = true;
 			}
-			else if(up)
+			else if(bottom)
 			{
 				pSegment->m_IsVertical = false;
-				if(!up_i)
-					up_i = -200;
-				pSegment->m_A = vec2(up_i*32,(j+1)*32);
+				if(!bottom_i)
+					bottom_i = -200;
+				pSegment->m_A = vec2(bottom_i*32,(j+1)*32);
 				pSegment->m_B = vec2(i*32,(j+1)*32);
 				pSegment++;
-				up = false;
+				bottom = false;
 			}
+
+			// Segment on the top of line j
 			if(((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+(j-1)*m_Width].m_Index <= 128 && m_pTiles[i+(j-1)*m_Width].m_Index & COLFLAG_SOLID)))
 			{
-				if(!down)
-					down_i = i;
-				down = true;
+				if(!top)
+					top_i = i;
+				top = true;
 			}
-			else if(down)
+			else if(top)
 			{
 				pSegment->m_IsVertical = false;
-				if(!down_i)
-					down_i = -200;
-				pSegment->m_A = vec2(down_i*32,j*32);
+				if(!top_i)
+					top_i = -200;
+				pSegment->m_A = vec2(top_i*32,j*32);
 				pSegment->m_B = vec2(i*32,j*32);
 				pSegment++;
-				down = false;
+				top = false;
 			}
 		}
-		if(up)
+		if(bottom)
 		{
 			pSegment->m_IsVertical = false;
-			if(!up_i)
-				up_i = -200;
-			pSegment->m_A = vec2(up_i*32,(j+1)*32);
+			if(!bottom_i)
+				bottom_i = -200;
+			pSegment->m_A = vec2(bottom_i*32,(j+1)*32);
 			pSegment->m_B = vec2((m_Width+200)*32,(j+1)*32);
 			pSegment++;
-			up = false;
+			bottom = false;
 		}
-		if(down)
+		if(top)
 		{
 			pSegment->m_IsVertical = false;
-			if(!down_i)
-				down_i = -200;
-			pSegment->m_A = vec2(down_i*32,j*32);
+			if(!top_i)
+				top_i = -200;
+			pSegment->m_A = vec2(top_i*32,j*32);
 			pSegment->m_B = vec2((m_Width+200)*32,j*32);
 			pSegment++;
-			down = false;
+			top = false;
 		}
 	}
 	// Vertical segments
 	for(int i = 1;i < m_Width-1; i++)
 	{
-		bool right = false;
 		bool left = false;
-		int right_j, left_j;
+		bool right = false;
+		int left_j, right_j;
 		for(int j = 0; j < m_Height; j++)
 		{
+			// Segment on the right side of column i
 			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i+1+j*m_Width].m_Index <= 128 && m_pTiles[i+1+j*m_Width].m_Index & COLFLAG_SOLID))
-			{
-				if(!left)
-					right_j = j;
-				left = true;
-			}
-			else if(left)
-			{
-				pSegment->m_IsVertical = true;
-				if(!left_j)
-					left_j = -200;
-				pSegment->m_A = vec2((i+1)*32,right_j*32);
-				pSegment->m_B = vec2((i+1)*32,j*32);
-				pSegment++;
-				left = false;
-			}
-			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i-1+j*m_Width].m_Index <= 128 && m_pTiles[i-1+j*m_Width].m_Index & COLFLAG_SOLID))
 			{
 				if(!right)
 					right_j = j;
@@ -227,35 +221,51 @@ void CCollision::BuildSegments()
 				pSegment->m_IsVertical = true;
 				if(!right_j)
 					right_j = -200;
-				pSegment->m_A = vec2(i*32,right_j*32);
-				pSegment->m_B = vec2(i*32,j*32);
+				pSegment->m_A = vec2((i+1)*32,right_j*32);
+				pSegment->m_B = vec2((i+1)*32,j*32);
 				pSegment++;
 				right = false;
 			}
-		}
-		if(left)
-		{
-			pSegment->m_IsVertical = true;
-			if(!left_j)
-				left_j = -200;
-			pSegment->m_A = vec2((i+1)*32,left_j*32);
-			pSegment->m_B = vec2((i+1)*32,(m_Height+200)*32);
-			pSegment++;
-			left = false;
+
+			// Segment on the left side of column i
+			if((m_pTiles[i+j*m_Width].m_Index <= 128 && m_pTiles[i+j*m_Width].m_Index & COLFLAG_SOLID) && !(m_pTiles[i-1+j*m_Width].m_Index <= 128 && m_pTiles[i-1+j*m_Width].m_Index & COLFLAG_SOLID))
+			{
+				if(!left)
+					left_j = j;
+				left = true;
+			}
+			else if(left)
+			{
+				pSegment->m_IsVertical = true;
+				if(!left_j)
+					left_j = -200;
+				pSegment->m_A = vec2(i*32,left_j*32);
+				pSegment->m_B = vec2(i*32,j*32);
+				pSegment++;
+				left = false;
+			}
 		}
 		if(right)
 		{
 			pSegment->m_IsVertical = true;
 			if(!right_j)
 				right_j = -200;
-			pSegment->m_A = vec2(i*32,right_j*32);
+			pSegment->m_A = vec2((i+1)*32,right_j*32);
+			pSegment->m_B = vec2((i+1)*32,(m_Height+200)*32);
+			pSegment++;
+		}
+		if(left)
+		{
+			pSegment->m_IsVertical = true;
+			if(!left_j)
+				left_j = -200;
+			pSegment->m_A = vec2(i*32,left_j*32);
 			pSegment->m_B = vec2(i*32,(m_Height+200)*32);
 			pSegment++;
-			right = false;
 		}
 	}
 	dbg_msg("collision","Allocate %d segments, use %d", m_SegmentCount, pSegment - m_pSegments);
-
+	// Sort by y-coordinate and x-coordinate
 	qsort(m_pSegments,HSegmentCount,sizeof(CSegment),SegmentComp);
 	qsort(m_pSegments+HSegmentCount,VSegmentCount,sizeof(CSegment),SegmentComp);
 }
@@ -335,7 +345,10 @@ int CCollision::IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *p
 int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *pOutBeforeCollision) const
 {
 	float T = 1;
-	if(CheckPoint(Pos0.x, Pos0.y))
+	vec2 Pos;
+	vec2 Dir = normalize(Pos1-Pos0);
+	// Check if the starting point is in a solid tile
+	if(CheckPoint(Pos0))
 	{
 		if(pOutCollision)
 			*pOutCollision = Pos0;
@@ -350,6 +363,7 @@ int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2
 		int i = 0, j = m_HSegmentCount;
 		if(Pos0.y < Pos1.y)
 		{
+			// Look for the first segment with y-coordinate >= Pos0.y
 			while(i+1<j)
 			{
 				int k = (i+j)>>1;
@@ -358,21 +372,25 @@ int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2
 				else
 					j = k;
 			}
-			i++;
-			while(i < m_HSegmentCount && m_pSegments[i].m_A.y <= Pos1.y)
+			while(j < m_HSegmentCount && m_pSegments[j].m_A.y <= Pos1.y)
 			{
-				float d1 = det(m_pSegments[i].m_A - Pos0, Pos1 - Pos0);
-				float d2 = det(m_pSegments[i].m_B - Pos0, Pos1 - Pos0);
-				if(d1*d2 < 0)
+				float d1 = det(m_pSegments[j].m_A - Pos0, Pos1 - Pos0);
+				float d2 = det(m_pSegments[j].m_B - Pos0, Pos1 - Pos0);
+				if(d1*d2 <= 0)
 				{
-					T = (m_pSegments[i].m_A.y-Pos0.y)*idy;
-					break;
+					float Temp = (m_pSegments[j].m_A.y-Pos0.y)*idy;
+					if(Temp < T)
+					{
+						T = Temp;
+						break;
+					}
 				}
-				i++;
+				j++;
 			}
 		}
-		else
+		else // Pos0.y > Pos1.y
 		{
+			// Look for the last segment with y-coordinate <= Pos0.y
 			while(i+1<j)
 			{
 				int k = (i+j)>>1;
@@ -381,17 +399,20 @@ int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2
 				else
 					j = k;
 			}
-			j--;
-			while(j >= 0 && m_pSegments[j].m_A.y > Pos1.y)
+			while(i >= 0 && m_pSegments[i].m_A.y > Pos1.y)
 			{
-				float d1 = det(m_pSegments[j].m_A - Pos0, Pos1 - Pos0);
-				float d2 = det(m_pSegments[j].m_B - Pos0, Pos1 - Pos0);
-				if(d1*d2 < 0)
+				float d1 = det(m_pSegments[i].m_A - Pos0, Pos1 - Pos0);
+				float d2 = det(m_pSegments[i].m_B - Pos0, Pos1 - Pos0);
+				if(d1*d2 <= 0)
 				{
-					T = (m_pSegments[j].m_A.y-Pos0.y)*idy;
-					break;
+					float Temp = (m_pSegments[i].m_A.y-Pos0.y)*idy;
+					if(Temp < T)
+					{
+						T = Temp;
+						break;
+					}
 				}
-				j--;
+				i--;
 			}
 		}
 	}
@@ -404,6 +425,7 @@ int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2
 		int i = m_HSegmentCount, j = m_SegmentCount;
 		if(Pos0.x < Pos1.x)
 		{
+			// Look for the first segment with x-coordinate >= Pos0.x
 			while(i+1<j)
 			{
 				int k = (i+j)>>1;
@@ -412,35 +434,11 @@ int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2
 				else
 					j = k;
 			}
-			i++;
-			while(i < m_SegmentCount && m_pSegments[i].m_A.x <= Pos1.x)
-			{
-				float d1 = det(m_pSegments[i].m_A - Pos0, Pos1 - Pos0);
-				float d2 = det(m_pSegments[i].m_B - Pos0, Pos1 - Pos0);
-				if(d1*d2 < 0)
-				{
-					T = (m_pSegments[i].m_A.x-Pos0.x)*idx;
-					break;
-				}
-				i++;
-			}
-		}
-		else
-		{
-			while(i+1<j)
-			{
-				int k = (i+j)>>1;
-				if(m_pSegments[k].m_A.x <= Pos0.x)
-					i = k;
-				else
-					j = k;
-			}
-			j--;
-			while(j >= m_HSegmentCount && m_pSegments[j].m_A.x > Pos1.x)
+			while(j < m_SegmentCount && m_pSegments[j].m_A.x <= Pos1.x)
 			{
 				float d1 = det(m_pSegments[j].m_A - Pos0, Pos1 - Pos0);
 				float d2 = det(m_pSegments[j].m_B - Pos0, Pos1 - Pos0);
-				if(d1*d2 < 0)
+				if(d1*d2 <= 0)
 				{
 					float Temp = (m_pSegments[j].m_A.x-Pos0.x)*idx;
 					if(Temp < T)
@@ -450,27 +448,64 @@ int CCollision::IntersectSegment(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2
 						break;
 					}
 				}
-				j--;
+				j++;
+			}
+		}
+		else // Pos1.x < Pos0.x
+		{
+			// Look for the last segment with x-coordinate <= Pos0.x
+			while(i+1<j)
+			{
+				int k = (i+j)>>1;
+				if(m_pSegments[k].m_A.x <= Pos0.x)
+					i = k;
+				else
+					j = k;
+			}
+			while(i >= m_HSegmentCount && m_pSegments[i].m_A.x >= Pos1.x)
+			{
+				float d1 = det(m_pSegments[i].m_A - Pos0, Pos1 - Pos0);
+				float d2 = det(m_pSegments[i].m_B - Pos0, Pos1 - Pos0);
+				if(d1*d2 <= 0)
+				{
+					float Temp = (m_pSegments[i].m_A.x-Pos0.x)*idx;
+					if(Temp < T)
+					{
+						T = Temp;
+						Vertical = true;
+						break;
+					}
+				}
+				i--;
 			}
 		}
 	}
 	if(T < 1)
 	{
 		T = clamp(T,0.f,1.f);
-		vec2 Pos = mix(Pos0,Pos1,T);
-		vec2 Dir = normalize(Pos1-Pos0);
+		Pos = mix(Pos0,Pos1,T);
+		// Return the real intersection, so it's a bit different from IntersectLine
+		// that returns the first point that is in a solid tile.
 		if(pOutCollision)
 			*pOutCollision = Pos;
+
+		// Get the tile...
+		int Index = GetTile(Pos.x,Pos.y);
+		if(!Index)
+			Index = GetTile(Pos.x+Dir.x,Pos.y+Dir.y);
+		if(!Index)
+			Index = GetTile(Pos.x+ ((Vertical) ? Dir.x : -Dir.x),Pos.y+((Vertical) ? -Dir.y : Dir.y));
+
+		if(Vertical)
+			Dir *= 0.5f / absolute(Dir.x) + 1.f;
+		else
+			Dir *= 0.5f / absolute(Dir.y) + 1.f;
+		// Only used for laser, previous point is compute by removing enough
+		// distance so that laser is not stuck in the solid tile (due to MovePoint)
 		if(pOutBeforeCollision)
-		{
-			if(Vertical)
-				Dir *= 0.5f / absolute(Dir.x) + 1.f;
-			else
-				Dir *= 0.5f / absolute(Dir.y) + 1.f;
 			*pOutBeforeCollision = Pos - Dir;
-		}
-		Pos += normalize(Pos1-Pos0)*16.f;
-		return GetCollisionAt(Pos.x, Pos.y);
+
+		return Index;
 	}
 	if(pOutCollision)
 		*pOutCollision = Pos1;
