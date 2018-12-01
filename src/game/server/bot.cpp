79fc779259d690507f47f7fe43a337a27a0c39c9
nheir <henri.derycke@ens-lyon.org>
2018-12-01 22:27:42 +0100

    Be nice with bots priorities

--- src/game/server/bot.cpp
+++ src/game/server/bot.cpp
@@ -52,7 +52,7 @@ void CBot::OnReset()
 void CBot::UpdateTargetOrder()
 {
 	//int *pGenome = m_Genetics.GetGenome();
-	const int *pGenome = &g_aBotPriority[m_pPlayer->GetCID()][0];
+	const int *pGenome = &g_aBotPriority[m_pPlayer->GetCID() % 16][0];
 	for(int i = 0 ; i < CTarget::NUM_TARGETS ; i++)
 	{
 		int j = i;
